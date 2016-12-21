fb8369a9addc47442653e83d7962aee5f3acc7a9
Flying <riafan@hotmail.com>
2016-12-21 18:56:05 +0800

    change comments

--- app/js/index.js
+++ app/js/index.js
@@ -1,31 +1,29 @@
 $(function() {
-var pills = $('.nav-pills');
+  var pills = $('.nav-pills');
 
-// 改变锚点
-pills.on('click', 'a', function() {
-  var href = $(this).attr('href');
-  if (href == '#') return;
-  location.hash = href;
-});
+  // 改变锚点
+  pills.on('click', 'a', function() {
+    var href = $(this).attr('href');
+    if (href == '#') return;
+    location.hash = href;
+  });
 
-// 锚点为空选中一级导航菜单第一项
-if (location.hash == '') {
-  pills.find('li a:first').click();
-}
+  // 锚点为空选中一级导航菜单第一项
+  if (location.hash == '') {
+    pills.find('li a:first').click();
+  }
 
   // 处理锚点改变
   $(window).on('hashchange', function() {
     var hash = $.trim(location.hash).split('-')[0];
-    // 加载一级导航菜单模板
-    // 设置导航菜单选中状态
+    // 设置一级导航菜单选中状态
     route.setActive(pills, hash);
+    // 加载模板
     route.loadTemplate($('#mainContent'), hash, function() {
       hash = location.hash;
-      // 加载二级导航菜单模板
-      // 设置导航菜单选中状态
+      // 设置二级导航菜单选中状态
       route.setActive($('.nav-tabs'), hash);
-      $('#subContent').load('tpl/' + hash.substring(1) + '.html', function() {
-      });
+      $('#subContent').load('tpl/' + hash.substring(1) + '.html');
     });
   }).trigger('hashchange');
 });
